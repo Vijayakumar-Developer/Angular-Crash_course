295012bbf868354581243eafd85801b0f5de97fb
Vijayakumar-Developer <vijai1993@gmail.com>
2024-06-17 14:16:20 +0530

    angularcrash_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang_crashmodule';
+  constructor(private toastr: ToastrService) {
+
+    this.toastr.success('Hello Vijay!', 'Toastr fun!');
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,CommonModule,
+    BrowserAnimationsModule,ToastrModule.forRoot(),
   ],
   providers: [
     provideClientHydration()
