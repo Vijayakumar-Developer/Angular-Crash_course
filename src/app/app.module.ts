d87c1f8e923b3a846812f4d51ad3912ea44e5fd0
Vijayakumar-Developer <vijai1993@gmail.com>
2024-06-18 18:32:42 +0530

    button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthLinksComponent } from './shared/auth-links/auth-links.component';
 import { SocialLinksComponent } from './shared/social-links/social-links.component';
 import { PhoneComponent } from './shared/phone/phone.component';
 import { EmailComponent } from './shared/email/email.component';
+import { ButttonComponent } from './shared/buttton/buttton.component';
 
 
 
@@ -31,7 +32,8 @@ import { EmailComponent } from './shared/email/email.component';
     AuthLinksComponent,
     SocialLinksComponent,
     PhoneComponent,
-    EmailComponent
+    EmailComponent,
+    ButttonComponent
   ],
   imports: [
     BrowserModule,RouterModule,AppRoutingModule
---
+++ src/app/shared/buttton/buttton.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-buttton',
+  templateUrl: './buttton.component.html',
+  styleUrl: './buttton.component.css'
+})
+export class ButttonComponent {
+ 
+  @Input('size')
+  size='S'; //S,M,L
+  shades='NA';//D,L,W,NA
+  animation='NA';//C,F,NA
+
+  constructor(){
+
+  }
+  ngOnInit():void{
+
+  }
+}
